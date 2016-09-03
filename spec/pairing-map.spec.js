30f98b2ee4fa251fb8c7df27848aa1466b3310b6
Diego Arruda <arruda@gmail.com>
2016-09-02 17:14:19 -0700

    [Diego] Create the JSON builder to be used as the input for the graph on UI.

--- spec/pairing-map.spec.js
+++ spec/pairing-map.spec.js
@@ -42,3 +42,73 @@ describe("Link beetween people checking", function() {
 	
   
 });
+
+describe("Generate data to UI.", function() {
+
+    it("Pairing Map should be created with no collaborators.", function() {
+        var pairingMap = new PairingMap();
+        expect(pairingMap.collaborators.length).toBe(0);
+    });
+
+    it("Should return the collaborator index on for added collaborator.", function() {
+        var pairingMap = new PairingMap();
+        pairingMap.addCollaborator('john');
+
+        expect(pairingMap.addCollaborator('roy')).toBe(1);
+    });
+
+    it("Should return the current collaborator index when collaborator is already added.", function() {
+        var pairingMap = new PairingMap();
+        pairingMap.addCollaborator('john');
+        pairingMap.addCollaborator('roy');
+        pairingMap.addCollaborator('bran');
+
+        expect(pairingMap.addCollaborator('bran')).toBe(2);
+    });
+
+    it("Should return collaborator index by ID.", function() {
+        var pairingMap = new PairingMap();
+        pairingMap.addCollaborator('john');
+
+        expect(pairingMap.getIndex('john')).toBe(0);
+    });
+
+    it("Should return -1 as the index when the collaborator is not found.", function() {
+        var pairingMap = new PairingMap();
+        pairingMap.addCollaborator('john');
+
+        expect(pairingMap.getIndex('roy')).toBe(-1);
+    });
+
+    it("Should return the collaborator by ID.", function() {
+        var pairingMap = new PairingMap();
+        pairingMap.addCollaborator('john', 'John Simpson', 'http://localhost/avatar/simpson.jpg', 1);
+
+        expect(pairingMap.getCollaborator('john')).toEqual({'id':'john','name':'John Simpson', 'imageUrl':'http://localhost/avatar/simpson.jpg', 'group':1});
+    });
+
+    it("Should build the pairing map.", function() {
+        var pairingMap = new PairingMap();
+        pairingMap.map = [
+            {"source":"john", "target":"bran", "pairings":1},
+            {"source":"roy", "target":"bran", "pairings":2}
+        ];
+        pairingMap.addLink('john', 'bob');
+
+        var expectValue = {
+          "nodes": [
+            {"name": "john", "group": 1},
+            {"name": "bran", "group": 1},
+            {"name": "roy", "group": 1},
+            {"name": "bob", "group": 1}
+          ],
+          "links": [
+            {"source": 0, "target": 1, "value": 1},
+            {"source": 2, "target": 1, "value": 2},
+            {"source": 0, "target": 3, "value": 1},
+            ]
+        };
+
+        expect(pairingMap.build()).toEqual(expectValue);
+    });
+});
--- src/pairing-map.js
+++ src/pairing-map.js
@@ -1,5 +1,7 @@
 function PairingMap() { 
+	var self = this;
 	this.map = [];
+	this.collaborators = [];
 
 	this.addLink = function(source, target) {
 		var link = this.getLink(source, target);
@@ -22,6 +24,56 @@ function PairingMap() {
 			return false;
 		});
 		return foundLink;
+	}
+
+	this.addCollaborator = function(id, name, imageUrl, group) {
+		var index = this.getIndex(id);
+		if (index == -1) {
+			index = this.collaborators.length;
+			this.collaborators.push({"id":id, "name": name, "imageUrl":imageUrl, "group":group});
+		}
+		return index;
+	}
+
+	this.getIndex = function(id) {
+		var index = -1;
+		for (var i = 0; i < this.collaborators.length; i++) {
+			var collaborator = this.collaborators[i];
+			if (collaborator.id == id) {
+				index = i;
+				break;
+			}
+		};
+		
+		return index;
+	}
+
+	this.getCollaborator = function(id) {
+		var collaborator;
+		var index = this.getIndex(id);
+
+		if (index != -1) {
+			collaborator = this.collaborators[index];
+		}
+		return collaborator;
+	}
+
+	this.build = function() {
+		var nodes = [];
+		var links = [];
+
+		this.map.forEach(function(link) {
+			var sourceIndex = self.addCollaborator(link.source, link.source, null, 1);
+			var targetIndex = self.addCollaborator(link.target, link.target, null, 1);
+
+			links.push({"source":sourceIndex,"target":targetIndex,"value":link.pairings});
+		});
+
+		this.collaborators.forEach(function(collaborator) {
+			nodes.push({"name": collaborator.name, "group": collaborator.group});
+		});
+
+		return {"nodes":nodes,"links":links};
 	}
 }
 
